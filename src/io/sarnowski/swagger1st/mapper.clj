77922734556c4ba1dfafe18b63b0a101affcc6d2
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-04-28 12:36:29 +0200

    added mapper tests and fixed mapper

--- src/io/sarnowski/swagger1st/mapper.clj
+++ src/io/sarnowski/swagger1st/mapper.clj
@@ -6,13 +6,13 @@
             [clojure.tools.logging :as log]
             [io.sarnowski.swagger1st.util.api :as api]))
 
-(defn- get-definition
+(defn get-definition
   "Resolves a $ref reference to its content."
   [r d]
   (let [path (rest (string/split r #"/"))]
     (get-in d path)))
 
-(defn- denormalize-refs
+(defn denormalize-refs
   "Searches for $ref objects and replaces those with their target."
   [definition]
   (let [check-ref (fn [element]
@@ -25,7 +25,7 @@
                        element))
                    definition)))
 
-(defn- inherit-map
+(defn inherit-map
   "Merges a map from parent to definition, overwriting keys with definition."
   [definition parent-definition map-name]
   (let [m (get definition map-name)
@@ -33,7 +33,7 @@
         merged (merge pm m)]
     (assoc definition map-name merged)))
 
-(defn- inherit-list
+(defn inherit-list
   "Denormalizes a collection, using the parent or replacing it with definition."
   [definition parent-definition col-name]
   (assoc definition col-name
@@ -41,7 +41,7 @@
                       col
                       (get parent-definition col-name))))
 
-(defn- conj-if-not
+(defn conj-if-not
   "Conjoins x to col if test-fn doesn't find an existing entry in col."
   [test-fn col x & xs]
   (let [col (if (empty? (filter (fn [y] (test-fn x y)) col))
@@ -51,7 +51,7 @@
       (recur test-fn col (first xs) (next xs))
       col)))
 
-(defn- inherit-list-elements
+(defn inherit-list-elements
   "Denormalizes a collection, replacing entries that are equal."
   [definition parent-definition col-name if-not-fn]
   (assoc definition col-name
@@ -60,12 +60,12 @@
                       (remove nil?
                               (conj-if-not if-not-fn d (first pd) (next pd))))))
 
-(defn- keys-equal?
+(defn keys-equal?
   "Compares two maps if both have the same given keys."
   [x y ks]
   (every? (fn [k] (= (get x k) (get y k))) ks))
 
-(defn- inherit-mimetypes
+(defn inherit-mimetypes
   "Inherit 'consumes' and 'produces' mimetypes if not defined."
   [definition parent-definition]
   (-> definition
@@ -73,7 +73,7 @@
       (inherit-list parent-definition "produces")
       (inherit-list parent-definition "security")))
 
-(defn- inherit-path-spec
+(defn inherit-path-spec
   "Denormalizes inheritance of parameters etc. from the path to operation."
   [definition parent-definition]
   (-> definition
@@ -83,12 +83,12 @@
       (inherit-list parent-definition "security")))
 
 (defn split-path
-  "Splits a / separated path into its segments and replaces all variable entries (e.g. {name}) with nil."
+  "Splits a / separated path into its segments."
   [path]
   (let [split (fn [^String s] (.split s "/"))]
     (-> path split rest)))
 
-(defn- variable-to-keyword
+(defn variable-to-keyword
   "Replaces a variable path segment (like /{username}/) with the variable name as keyword (like :username)."
   [seg]
   (if-let [variable-name (second (re-matches #"\{(.*)\}" seg))]
@@ -97,19 +97,16 @@
     ; no variable found, return original segment
     seg))
 
-(defn- create-request-tuple
+(defn create-request-tuple
   "Generates easier to digest request tuples from operations and paths."
   [operation operation-definition path path-definition]
   (let [keyword-path (->> path split-path (map variable-to-keyword))
-        key-path (map #(if (keyword? %) nil %) keyword-path)
         definition (inherit-path-spec
                      operation-definition
-                     path-definition)
-        template {:operation operation
-                  :path      keyword-path}]
-    [key-path (assoc definition :template template)]))
+                     path-definition)]
+    [[operation keyword-path] definition]))
 
-(defn- extract-requests
+(defn extract-requests
   "Extracts request-key->operation-definition from a swagger definition."
   [definition]
   (let [inheriting-key? #{"parameters" "consumes" "produces" "schemes" "security"}]
@@ -135,26 +132,26 @@
       denormalize-refs
       extract-requests))
 
-(defn- path-machtes?
+(defn path-machtes?
   "Matches a template path with a real path. Paths are provided as collections of their segments. If the template has
-   a nil value, it is a dynamic segment."
+   a keyword value, it is a dynamic segment."
   [path-template path-real]
   (when (= (count path-template) (count path-real))
     (let [pairs (map #(vector %1 %2) path-template path-real)
           pair-matches? (fn [[t r]] (or (keyword? t) (= t r)))]
       (every? pair-matches? pairs))))
 
-(defn- request-matches?
+(defn request-matches?
   "Checks if the given request matches a defined swagger-request."
-  [definition request]
-  (and (= (-> definition :template :operation) (-> request :request-method name))
-       (path-machtes? (-> definition :template :path) (-> request :uri split-path))))
+  [[operation path-template] request]
+  (and (= operation (-> request :request-method name))
+       (path-machtes? path-template (-> request :uri split-path))))
 
 (defn lookup-request
   "Creates a function that can do efficient lookups of requests."
   [requests request]
   (->> requests
-       (filter (fn [[_ definition]] (request-matches? definition request)))
+       (filter (fn [[template _]] (request-matches? template request)))
        ; if we have multiple matches then its not well defined, just choose the first
        first))
 
---
+++ test/io/sarnowski/swagger1st/mapper_test.clj
@@ -0,0 +1,72 @@
+(ns io.sarnowski.swagger1st.mapper-test
+  (:require [clojure.test :refer :all]
+            [io.sarnowski.swagger1st.mapper :as m]))
+
+; TODO tests for definition inheritance
+
+;; path templating and matching
+
+(deftest variable-keyword-extraction
+  (is (= :foo (m/variable-to-keyword "{foo}")))
+  (is (= "foo" (m/variable-to-keyword "foo"))))
+
+(deftest path-splitting
+  (is (= [] (m/split-path "/")))
+  (is (= ["foo"] (m/split-path "/foo")))
+  (is (= ["foo" "bar"] (m/split-path "/foo/bar"))))
+
+(deftest path-matching
+  (is (m/path-machtes? '() '()))
+  (is (not (m/path-machtes? '() '("foo"))))
+  (is (m/path-machtes? '("foo") '("foo")))
+  (is (not (m/path-machtes? '("foo") '())))
+  (is (m/path-machtes? '(:x) '("foo")))
+  (is (m/path-machtes? '("foo" :x) '("foo" "bar")))
+  (is (m/path-machtes? '("foo" :x "bar") '("foo" "baz" "bar")))
+  (is (not (m/path-machtes? '("foo" :x "bar") '("fuzz" "baz" "bar")))))
+
+(deftest create-request-tuple
+  (is (= ["get" []] (first (m/create-request-tuple "get" {} "/" {}))))
+  (is (= ["get" ["foo"]] (first (m/create-request-tuple "get" {} "/foo" {}))))
+  (is (= ["get" ["foo" "bar"]] (first (m/create-request-tuple "get" {} "/foo/bar" {}))))
+  (is (= ["get" ["foo" :bar "baz"]] (first (m/create-request-tuple "get" {} "/foo/{bar}/baz" {})))))
+
+(deftest request-matches?
+  (is (m/request-matches? ["get" []] {:request-method :get :uri "/"}))
+  (is (m/request-matches? ["get" ["foo"]] {:request-method :get :uri "/foo"}))
+  (is (m/request-matches? ["post" [:foo "bar"]] {:request-method :post :uri "/foo/bar"})))
+
+;; definition to lookup structure
+
+(deftest extract-requests
+  (is (= #{["get" []]
+           ["post" []]
+
+           ["get" ["foo" :bar]]
+           ["post" ["foo" :bar]]
+           ["delete" ["foo" :bar]]}
+         (->>
+           (m/extract-requests
+             {"paths" {
+                       "/"
+                       {"get"  {}
+                        "post" {}}
+
+                       "/foo/{bar}"
+                       {"get"    {}
+                        "post"   {}
+                        "delete" {}}}})
+           (map (fn [[k _]] k))
+           (into #{})))))
+
+(deftest lookup-request
+  (let [requests {["get" []] :get
+                  ["post" []] :post
+
+                  ["get" ["foo" :bar]] :get-foo
+                  ["post" ["foo" :bar]] :post-foo
+                  ["delete" ["foo" :bar]] :delete-foo}]
+
+  (is (= :get (second (m/lookup-request requests {:request-method :get :uri "/"}))))
+  (is (= :get-foo (second (m/lookup-request requests {:request-method :get :uri "/foo/baz"}))))
+  (is (= :delete-foo (second (m/lookup-request requests {:request-method :delete :uri "/foo/baz"}))))))
