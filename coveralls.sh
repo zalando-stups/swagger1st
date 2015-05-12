a6bd43eeb6797841a28cafbfa0f1b437c0881230
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-05-12 09:14:12 +0200

    fixed cloverage version

--- coveralls.sh
+++ coveralls.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-CLOVERAGE_VERSION='1.0.6' lein cloverage -o cov --coveralls
+CLOVERAGE_VERSION='1.0.5' lein cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
