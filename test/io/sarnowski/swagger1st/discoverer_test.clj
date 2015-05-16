7fa852292483e47ee843fac4d096c76335d93f45
Tobias Sarnowski <tobias@sarnowski.io>
2015-05-16 23:37:31 +0200

    tests for discovery endpoints

--- test/io/sarnowski/swagger1st/discoverer_test.clj
+++ test/io/sarnowski/swagger1st/discoverer_test.clj
@@ -13,4 +13,16 @@
     (is (= discovery)
         {:schema_url  "/swagger.json"
          :schema_type "swagger-2.0"
-         :ui_url      "/ui/"})))
+         :ui_url      "/ui/"}))
+
+  (let [result (app (mock/request :get "/swagger.json"))
+        definition (json/read-str (:body result))]
+    (is (= "2.0" (get definition "swagger"))))
+
+  (let [result (app (mock/request :get "/ui/"))
+        html (:body result)]
+    (is (.contains html "html")))
+
+  (let [result (app (mock/request :get "/ui/swagger-ui.min.js"))
+        js (slurp (:body result))]
+    (is (.contains js "prototype"))))
