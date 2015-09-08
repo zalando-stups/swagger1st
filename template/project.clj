787a37f25b2a2fda31a791c46e9eacc9002906c5
Tobias Sarnowski <tobias@sarnowski.io>
2015-09-08 22:13:55 +0200

    added missing :scm information for clojars

--- template/project.clj
+++ template/project.clj
@@ -2,6 +2,8 @@
   :description "Leiningen template for creating a swagger1st app."
   :url "https://github.com/sarnowski/swagger1st"
 
+  :scm {:url "git@github.com:sarnowski/swagger1st.git"}
+
   :license {:name "ISC License"
             :url "http://opensource.org/licenses/ISC"
             :distribution :repo}
