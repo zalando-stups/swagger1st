1e118513fd9836493e34a16eb7b5b8172ec1761f
Tobias Sarnowski <tobias@sarnowski.io>
2015-09-08 22:12:34 +0200

    fixed version for template

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject swagger1st/lein-template "0.1.0-SNAPSHOT"
+(defproject swagger1st/lein-template "0.15.0"
   :description "Leiningen template for creating a swagger1st app."
   :url "https://github.com/sarnowski/swagger1st"
 
@@ -6,13 +6,4 @@
             :url "http://opensource.org/licenses/ISC"
             :distribution :repo}
 
-  :eval-in-leiningen true
-
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "template-"]
-                  ["deploy"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]])
+  :eval-in-leiningen true)
