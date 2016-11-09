56d3de5542290020c556e0f2dceb14166031ecb6
Andre Hartmann <andre.hartmann@zalando.de>
2016-11-09 11:16:52 +0100

    Version 0.22.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando/swagger1st "0.22.1-SNAPSHOT"
+(defproject org.zalando/swagger1st "0.22.1"
   :description "A ring handler that does routing based on a swagger definition."
   :url "https://github.com/zalando/swagger1st"
 
