e07f6108689234b6eb00a16121b6ec9f0d035f36
Andre Hartmann <andre.hartmann@zalando.de>
2017-07-05 13:33:30 +0200

    template: update dependencies

--- template/resources/leiningen/new/swagger1st/component/project.clj
+++ template/resources/leiningen/new/swagger1st/component/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.0"]
-                 [org.zalando/swagger1st "0.22.0"]
-                 [com.stuartsierra/component "0.3.1"]])
+                 [ring "1.6.1"]
+                 [org.zalando/swagger1st "0.25.0"]
+                 [com.stuartsierra/component "0.3.2"]])
--- template/resources/leiningen/new/swagger1st/simple/project.clj
+++ template/resources/leiningen/new/swagger1st/simple/project.clj
@@ -1,8 +1,8 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.0"]
-                 [org.zalando/swagger1st "0.22.0"]]
+                 [ring "1.6.1"]
+                 [org.zalando/swagger1st "0.25.0"]]
 
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.12.0"]]
 
   :ring {:handler {{name}}.core/app})
