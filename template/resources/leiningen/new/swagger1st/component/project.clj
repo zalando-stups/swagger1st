14330fe1b569a32f70576d87bdb9bf0331433fc5
André Menneken <andre@zalando.de>
2019-02-19 11:47:24 +0100

    upgrade template to 0.26.0

--- template/resources/leiningen/new/swagger1st/component/project.clj
+++ template/resources/leiningen/new/swagger1st/component/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.1"]
-                 [org.zalando/swagger1st "0.25.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.6.3"]
+                 [org.zalando/swagger1st "0.26.0"]
                  [com.stuartsierra/component "0.3.2"]])
--- template/resources/leiningen/new/swagger1st/simple/project.clj
+++ template/resources/leiningen/new/swagger1st/simple/project.clj
@@ -1,8 +1,8 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.1"]
-                 [org.zalando/swagger1st "0.25.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.6.3"]
+                 [org.zalando/swagger1st "0.26.0"]]
 
-  :plugins [[lein-ring "0.12.0"]]
+  :plugins [[lein-ring "0.12.5"]]
 
   :ring {:handler {{name}}.core/app})
