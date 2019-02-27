33588f0e323954ec179c35941e2fbc0fc72d4f73
André Menneken <andre@zalando.de>
2019-02-27 10:55:53 +0100

    upgrade template to 0.27.0

--- template/resources/leiningen/new/swagger1st/component/project.clj
+++ template/resources/leiningen/new/swagger1st/component/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.6.3"]
-                 [org.zalando/swagger1st "0.26.0"]
+                 [org.zalando/swagger1st "0.27.0"]
                  [com.stuartsierra/component "0.3.2"]])
